a12528a07f31f22e481e8f4b43491b61b6c8d3a3
7ssSalha <husseinsoheil1999@hotmail.com>
2020-03-02 13:40:55 +0200

    go go go

--- test/cal/Testcase.java
+++ test/cal/Testcase.java
@@ -1,13 +1,17 @@
 package cal;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.*;
+import org.junit.Test;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+//import static org.junit.Assert.*;
+//
+//import org.junit.*;
+//
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
 
 import cal.Calculator;
 
